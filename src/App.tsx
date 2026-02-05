5376e73a8e79024d32f4598d2742dc0f66b70d1d
Hanna Torianyk <an.mol4anova@gmail.com>
2026-02-05 17:45:41 +0200

    Added <html class="has-navbar-fixed-top"

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { TabsPage } from './TabsPage';
 export const App = () => {
   return (
     <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
       <nav
         className="navbar is-light is-fixed-top is-mobile has-shadow"
         data-cy="Nav"
